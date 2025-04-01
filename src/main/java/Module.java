991b43f626b65bedf45e45551f1ca9ab20edc7de
Irenevn16 <irenevn16@gmail.com>
2025-04-01 16:12:26 +0200

    odule v2

--- src/main/java/Module.java
+++ src/main/java/Module.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class Module {
+    public static void main(String[] args) {
+
+        Module operaciones = new Module();
+
+        System.out.println("9 % 3: " + operaciones.calculoModulo(9, 3));
+        System.out.println("|10.5|:  " + operaciones.valorAbsoluto(10.5));
+
+    }
+    public int calculoModulo(int a, int b) {
+        return a % b;
+    }
+    public double valorAbsoluto(double a) {
+        if (a < 0) {
+            return -a;
+        } else {
+            return a;
+        }
+
+    }
 }
